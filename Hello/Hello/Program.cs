07a7b62af6c1a96c482a7d52d3576f8dee306386
U-Hashimoto-PC\Hashimoto <Hashimoto@Hashimoto-PC.(none)>
2013-12-12 23:41:09 +0900

    modifired hello's class

--- Hello/Hello/Program.cs
+++ Hello/Hello/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace Hello
 {
@@ -11,75 +10,27 @@ namespace Hello
     {
         static void Main(string[] args)
         {
-            /*
+
             int test = 1;
             string str;
             int[] ahoaho = new int[3];
 
 
             Console.WriteLine("Hello! C# World!");
-            Console.WriteLine("Number:{0}",test);
+            Console.WriteLine("Number:{0}", test);
             Console.WriteLine("てきとーな文字列を入力してください");
             str = Console.ReadLine();
             Console.WriteLine("Inputted:{0}", str);
             test = int.Parse(str);
             Console.WriteLine("Parsed:{0}", test);
 
-            for(test = 0;test<3;test++)
+            for (test = 0; test < 3; test++)
             {
                 ahoaho[test] = int.Parse(Console.ReadLine());
             }
 
-            Console.WriteLine("{0} {1} {2}", ahoaho[0] ,ahoaho[1], ahoaho[2]);
-            */
+            Console.WriteLine("{0} {1} {2}", ahoaho[0], ahoaho[1], ahoaho[2]);
 
-            Ham IMX = new Ham();
-
-            IMX.WriteCallsign();
-            IMX.ReadCallsign();
-
-        }
-    }
-
-    class Ham
-    {
-        private int Band;
-        private string callsign;
-
-       public void WriteCallsign()
-       {
-           callsign = Console.ReadLine();
-       }
-
-       public void ReadCallsign()
-       {
-           Console.WriteLine(callsign);
-       }
-    }
-
-    class botan
-    {
-        void botan()
-        {
-            DialogResult result = MessageBox.Show(
-                "ボタンを押してください。", "",
-                MessageBoxButtons.YesNo,
-                  MessageBoxIcon.Question);
-
-            if (result == DialogResult.Yes)
-            {
-                MessageBox.Show(
-                    "[はい] が押されました", "",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-            }
-            else if (result == DialogResult.No)
-            {
-                MessageBox.Show(
-                    "[いいえ] が押されました", "",
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Information);
-            }
         }
     }
 }
