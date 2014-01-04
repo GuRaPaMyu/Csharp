7436153012dd8756582b846da204e80e606b7d88
Hashi_Main <jp7fkf@gmail.com>
2014-01-04 11:42:59 +0900

    add size changed event. can resize picture box size by this event

--- Tsst/Tsst/Form1.Designer.cs
+++ Tsst/Tsst/Form1.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
       this.button1 = new System.Windows.Forms.Button();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
       this.SuspendLayout();
       // 
       // button1
       // 
-      this.button1.Location = new System.Drawing.Point(109, 191);
+      this.button1.Location = new System.Drawing.Point(203, 292);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(75, 23);
       this.button1.TabIndex = 0;
@@ -41,14 +43,28 @@
       this.button1.UseVisualStyleBackColor = true;
       this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Mouse_Clicked);
       // 
+      // pictureBox1
+      // 
+      this.pictureBox1.BackColor = System.Drawing.Color.Blue;
+      this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.Size = new System.Drawing.Size(461, 209);
+      this.pictureBox1.TabIndex = 1;
+      this.pictureBox1.TabStop = false;
+      // 
       // Form1
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(284, 262);
+      this.ClientSize = new System.Drawing.Size(485, 327);
+      this.Controls.Add(this.pictureBox1);
       this.Controls.Add(this.button1);
       this.Name = "Form1";
       this.Text = "TestProject";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.ClientSizeChanged += new System.EventHandler(this.CliantSizeChanged);
+      this.Paint += new System.Windows.Forms.PaintEventHandler(this.Paint_Form);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
       this.ResumeLayout(false);
 
         }
@@ -56,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Tsst/Tsst/Form1.cs
+++ Tsst/Tsst/Form1.cs
@@ -12,6 +12,8 @@ namespace Tsst
 {
     public partial class Form1 : Form
     {
+        private int i = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,8 +21,32 @@ namespace Tsst
 
         private void Mouse_Clicked(object sender, MouseEventArgs e)
         {
+          i++;
           MessageBox.Show("botton was clicked!", "ahoho", MessageBoxButtons.OK, MessageBoxIcon.Information);
-          
+          Invalidate();          
+        }
+
+        private void Paint_Form(object sender, PaintEventArgs e)
+        {
+          Graphics g;
+          g = e.Graphics;
+
+          Font fnt = new Font("ＭＳ　明朝", 10);
+          string str;
+          str = string.Format("test siteru {0}", i);
+
+          g.DrawString(str, fnt, SystemBrushes.WindowText, 0.0F, 0.0F);
+        }
+
+        private void CliantSizeChanged(object sender, EventArgs e)
+        {
+          pictureBox1.SetBounds(12, 12, ClientSize.Width - 24, ClientSize.Height - 100);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
 
     }
