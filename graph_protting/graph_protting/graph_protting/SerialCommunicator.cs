b0d239c4d69ea2ae1a0b76a033f3ace8919fd3c5
GuRaPamyu <jp7fkf@gmail.com>
2014-04-04 02:20:31 +0900

    add serial receiving event handler

--- graph_protting/graph_protting/graph_protting/SerialCommunicator.cs
+++ graph_protting/graph_protting/graph_protting/SerialCommunicator.cs
@@ -49,5 +49,15 @@ namespace graph_protting
       }
     }
 
+    private static void DataReceivedHandler(
+                        object sender,
+                        SerialDataReceivedEventArgs e)
+    {
+      SerialPort sp = (SerialPort)sender;
+      string indata = sp.ReadExisting();
+      Console.WriteLine("Data Received:");
+      Console.Write(indata);
+    }
+
   }
 }
